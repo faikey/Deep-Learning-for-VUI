93a87eb7f8308ca6cc5a99d8820dd3fec60baed6
knights207210 <xuha2442@colorado.edu>
2019-05-07 19:17:34 -0600

    skip-gram

---
+++ skip-gram/Skip-Gram.py
@@ -0,0 +1,213 @@
+import time
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+import utils
+from urllib.request import urlretrieve
+from os.path import isfile, isdir
+from tqdm import tqdm
+import zipfile
+from urllib.request import urlretrieve
+from os.path import isfile, isdir
+from tqdm import tqdm
+import zipfile
+from collections import Counter
+import random
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+
+df = pd.read_csv('train.csv')
+txt=''
+for index, row in df.iterrows():
+    txt = txt + ' ' + row['response']
+
+
+words = utils.preprocess(txt)
+print(words[:30])
+
+print("Total words: {}".format(len(words)))
+print("Unique words: {}".format(len(set(words))))
+
+vocab_to_int, int_to_vocab = utils.create_lookup_tables(words)
+int_words = [vocab_to_int[word] for word in words]
+
+
+np.random.seed(123)
+
+def sub_sampling(int_words, threshold):
+    word_counter = Counter(int_words)
+    total_count = len(int_words)
+    freq= {word: count/total_count for word,count in word_counter.items()}
+    p_drop ={word:(1-np.sqrt(threshold/ freq[word])) for word in word_counter}
+    trained_words = [word for word in int_words if p_drop[word]<np.random.random()]
+    return trained_words
+
+train_words = sub_sampling(int_words, threshold= 1e-5)
+
+
+
+
+def get_target(words, idx, window_size=5):
+
+    
+    R = np.random.randint(1, window_size+1)
+    start = idx - R if (idx - R) > 0 else 0
+    stop = idx + R
+    target_words = set(words[start:idx] + words[idx+1:stop+1])
+    
+    return list(target_words)
+
+
+
+def get_batches(words, batch_size, window_size=5):
+    
+    n_batches = len(words)//batch_size
+    
+    words = words[:n_batches*batch_size]
+    
+    for idx in range(0, len(words), batch_size):
+        x, y = [], []
+        batch = words[idx:idx+batch_size]
+        for ii in range(len(batch)):
+            batch_x = batch[ii]
+            batch_y = get_target(batch, ii, window_size)
+            y.extend(batch_y)
+            x.extend([batch_x]*len(batch_y))
+        yield x, y
+    
+
+
+train_graph = tf.Graph()
+with train_graph.as_default():
+    inputs = tf.placeholder(tf.int32, [None], name = 'inputs')
+    labels = tf.placeholder(tf.int32, [None, None], name='labels')
+
+
+
+
+n_vocab = len(int_to_vocab)
+n_embedding =  200             
+with train_graph.as_default():
+    
+
+    
+    embedding = tf.Variable(tf.random_uniform([n_vocab,n_embedding],-1, 1))   
+    embed = tf.nn.embedding_lookup(embedding, inputs)    
+
+
+n_sampled = 100
+with train_graph.as_default():
+    softmax_w = tf.Variable(tf.truncated_normal([n_vocab, n_embedding], stddev=0.1))
+    softmax_b = tf.Variable(tf.zeros(n_vocab))
+    
+    loss = tf.nn.sampled_softmax_loss(softmax_w, softmax_b, labels, embed, n_sampled, n_vocab)
+    
+    cost = tf.reduce_mean(loss)
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+
+
+
+
+with train_graph.as_default():
+   
+    
+    valid_size = 16              
+    valid_window = 100
+    
+  
+    valid_examples = np.array(random.sample(range(valid_window), valid_size//2))
+    valid_examples = np.append(valid_examples, 
+                               random.sample(range(1000,1000+valid_window), valid_size//2))
+
+    valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+    
+
+    norm = tf.sqrt(tf.reduce_sum(tf.square(embedding), 1, keep_dims=True))
+    normalized_embedding = embedding / norm
+    valid_embedding = tf.nn.embedding_lookup(normalized_embedding, valid_dataset)
+    similarity = tf.matmul(valid_embedding, tf.transpose(normalized_embedding))
+
+
+
+get_ipython().system('mkdir checkpoints')
+
+
+
+
+epochs = 1
+batch_size = 1000
+window_size = 10
+
+with train_graph.as_default():
+    saver = tf.train.Saver()
+
+with tf.Session(graph=train_graph) as sess:
+    iteration = 1
+    loss = 0
+    sess.run(tf.global_variables_initializer())
+
+    for e in range(1, epochs+1):
+        batches = get_batches(train_words, batch_size, window_size)
+        start = time.time()
+        for x, y in batches:
+            
+            feed = {inputs: x,
+                    labels: np.array(y)[:, None]}
+            train_loss, _ = sess.run([cost, optimizer], feed_dict=feed)
+            
+            loss += train_loss
+            
+            if iteration % 100 == 0: 
+                end = time.time()
+                print("Epoch {}/{}".format(e, epochs),
+                      "Iteration: {}".format(iteration),
+                      "Avg. Training loss: {:.4f}".format(loss/100),
+                      "{:.4f} sec/batch".format((end-start)/100))
+                loss = 0
+                start = time.time()
+            
+            if iteration % 1000 == 0:
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = int_to_vocab[valid_examples[i]]
+                    top_k = 8 # number of nearest neighbors
+                    nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                    log = 'Nearest to %s:' % valid_word
+                    for k in range(top_k):
+                        close_word = int_to_vocab[nearest[k]]
+                        log = '%s %s,' % (log, close_word)
+                    print(log)
+            
+            iteration += 1
+    save_path = saver.save(sess, "checkpoints/text8.ckpt")
+    embed_mat = sess.run(normalized_embedding)
+
+
+with train_graph.as_default():
+    saver = tf.train.Saver()
+
+with tf.Session(graph=train_graph) as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('checkpoints'))
+    embed_mat = sess.run(embedding)
+
+
+get_ipython().magic('matplotlib inline')
+get_ipython().magic("config InlineBackend.figure_format = 'retina'")
+
+
+
+viz_words = 500
+tsne = TSNE()
+embed_tsne = tsne.fit_transform(embed_mat[:viz_words, :])
+
+
+
+fig, ax = plt.subplots(figsize=(25, 25))
+for idx in range(viz_words):
+    plt.scatter(*embed_tsne[idx, :], color='steelblue')
+    plt.annotate(int_to_vocab[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha=0.7)
+plt.savefig('cluster.png')
+plt.show()
