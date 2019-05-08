cd4b56f8d7035383021aa8087e6caca7c9ddcadb
knights207210 <xuha2442@colorado.edu>
2019-05-07 18:37:56 -0600

    dataset and crawler codes

---
+++ crawler codes/alexacrawler.py
@@ -0,0 +1,213 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+
+driver = webdriver.Chrome("/Users/hanxu/Downloads/chromedriver")
+#driver.fullscreen_window()
+driver.get('https://developer.amazon.com/alexa/console/ask/test/amzn1.ask.skill.b78c5496-3dda-4085-ab6f-c51dfe3c785c/development/en_US/')
+
+username = ""
+password = ""
+
+elem_email=driver.find_element_by_id("ap_email")
+elem_email.send_keys(username)
+elem_ps = driver.find_element_by_id("ap_password")
+elem_ps.send_keys(password)
+elem_si = driver.find_element_by_id("signInSubmit")
+elem_si.click()
+
+
+datas = ['name','question','response']
+with open('music_open_help_stop_response1.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(datas)
+
+    with open("Workbook6.csv") as skill:
+        reader = csv.reader(skill)
+        firstline = True
+        for r in reader:
+            if firstline:
+                firstline = False
+                continue
+            if int(r[1]) >=3:
+                j = str(r[0])
+                row = []
+                row.append(j)
+                command = "Alexa, open "+ j
+                row.append(command)
+
+                elem = driver.find_element_by_class_name("askt-utterance__input")
+                elem.send_keys(command)
+    #enter = driver.find_element_by_class_name("askt-utterance")
+    #enter.send_keys(Keys.ENTER)
+                elem.send_keys(Keys.ENTER)
+                try:
+                    content = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--active-response")))
+                    #print(content.text)
+                    if not content.text:
+                        driver.get('https://developer.amazon.com/alexa/console/ask/test/amzn1.ask.skill.b78c5496-3dda-4085-ab6f-c51dfe3c785c/development/en_US/')
+                        time.sleep(3)
+                        elem = driver.find_element_by_class_name("askt-utterance__input")
+                        elem.send_keys(command)
+                        elem.send_keys(Keys.ENTER)
+                        try:
+                            content = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--response")))
+                            time.sleep(3)
+                            first = True
+                            for i in range(len(content)):
+                                if first:
+                                    row.append(i.text)
+                                    first = False
+                                else:
+                                    row[2] += i.text
+                        except:
+                            pass
+                        try:
+                            content_new = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--active-response")))
+                            row[2] += content_new.text
+                        except:
+                            pass
+                    else:
+                        row.append(content.text)
+    #content = driver.find_element_by_css_selector(".askt-dialog__message.askt-dialog__message--active-response")
+                except:
+                    row.append("")
+                    driver.get('https://developer.amazon.com/alexa/console/ask/test/amzn1.ask.skill.b78c5496-3dda-4085-ab6f-c51dfe3c785c/development/en_US/')
+                    time.sleep(3)
+                    elem = driver.find_element_by_class_name("askt-utterance__input")
+                    elem.send_keys(command)
+                    elem.send_keys(Keys.ENTER)
+                    try:
+                        content = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--response")))
+                        time.sleep(3)
+                        for i in range(len(content)):
+                            row[2] += i.text
+                    except:
+                        pass
+                    try:
+                        content_new = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--active-response")))
+                        row[2] += content_new.text
+                    except:
+                        pass
+    #print(content)
+                time.sleep(3)
+                writer.writerow(row)
+                if len(row) != 3:
+                    row.append("")
+
+                if row[2] != "" and "Do you want to open it?" in str(row[2]):
+                    row = []
+                    row.append(j)
+                    command = "Yes"
+                    row.append(command)
+
+                    elem = driver.find_element_by_class_name("askt-utterance__input")
+                    elem.send_keys(command)
+    #enter = driver.find_element_by_class_name("askt-utterance")
+    #enter.send_keys(Keys.ENTER)
+                    elem.send_keys(Keys.ENTER)
+                    try:
+                        content = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--active-response")))
+                    #print(content.text)
+                        row.append(content.text)
+    #content = driver.find_element_by_css_selector(".askt-dialog__message.askt-dialog__message--active-response")
+                    except:
+                        row.append("")
+                        driver.get('https://developer.amazon.com/alexa/console/ask/test/amzn1.ask.skill.b78c5496-3dda-4085-ab6f-c51dfe3c785c/development/en_US/')
+                        time.sleep(3)
+                        elem = driver.find_element_by_class_name("askt-utterance__input")
+                        elem.send_keys(command)
+                        elem.send_keys(Keys.ENTER)
+                        try:
+                            content = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--response")))
+                            time.sleep(3)
+                            for i in range(len(content)):
+                                row[2] += i.text
+                        except:
+                            pass
+                        try:
+                            content_new = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--active-response")))
+                            row[2] += content_new.text
+                        except:
+                            pass
+    #print(content)
+                    time.sleep(3)
+                    writer.writerow(row)
+
+                row = []
+                row.append(j)        
+                command = "help"
+                row.append(command)
+
+                elem = driver.find_element_by_class_name("askt-utterance__input")
+                elem.send_keys(command)
+    #enter = driver.find_element_by_class_name("askt-utterance")
+    #enter.send_keys(Keys.ENTER)
+                elem.send_keys(Keys.ENTER)
+
+                try:
+                    content = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--active-response")))
+                    #print(content.text)
+                    row.append(content.text)   
+        #content = driver.find_element_by_css_selector(".askt-dialog__message.askt-dialog__message--active-response")
+                except:
+                    row.append("")
+    #print(content)
+                time.sleep(3)
+                writer.writerow(row)
+
+                row = []
+                row.append(j)        
+                command = "stop"
+                row.append(command)
+
+                elem = driver.find_element_by_class_name("askt-utterance__input")
+                elem.send_keys(command)
+    #enter = driver.find_element_by_class_name("askt-utterance")
+    #enter.send_keys(Keys.ENTER)
+                elem.send_keys(Keys.ENTER)
+                try:
+                    content = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--active-response")))
+                    #print(content.text)
+                    row.append(content.text)
+    #content = driver.find_element_by_css_selector(".askt-dialog__message.askt-dialog__message--active-response")
+                except:
+                    row.append("")
+                time.sleep(3)
+                writer.writerow(row)
+
+                row = []
+                row.append(j)        
+                command = "quit"
+                row.append(command)
+
+                elem = driver.find_element_by_class_name("askt-utterance__input")
+                elem.send_keys(command)
+    #enter = driver.find_element_by_class_name("askt-utterance")
+    #enter.send_keys(Keys.ENTER)
+                elem.send_keys(Keys.ENTER)
+                try:
+                    content = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".askt-dialog__message.askt-dialog__message--active-response")))
+                    #print(content.text)
+                    row.append(content.text)
+    #content = driver.find_element_by_css_selector(".askt-dialog__message.askt-dialog__message--active-response")
+                except:
+                    row.append("")
+                
+    #print(content)
+                time.sleep(3)
+                writer.writerow(row)
+            else: 
+                break
+
+############################### one open-help-stop loop ######################################################
+
+
+
+
+
+
